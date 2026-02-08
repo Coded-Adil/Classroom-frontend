902db1cc5dfe29f8e63ff2a0bcdd41e78414f81e
Coded-Adil <itsadil2022@gmail.com>
2026-02-08 16:32:29 +0600

    implement data provider

---
+++ src/constants/index.ts
@@ -0,0 +1,14 @@
+export const DEPARTMENTS = [
+    'CS', 
+    'Math', 
+    'English',
+    'History',
+    'Physics',
+    'Chemistry',
+    'Biology',
+]
+
+export const DEPARTMENT_OPTIONS = DEPARTMENTS.map((dept) => ({
+    value: dept,
+    label: dept,
+}))
